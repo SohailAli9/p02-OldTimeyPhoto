f0ce251a9b5541d84624a1a8b25623b2b79a4783
SohailAli9 <salsamahi@mail.csuchico.edu>
2017-10-16 21:52:15 -0700

    Project 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <vector>
-#include <string>
 #include <iostream>
 #include "bitmap.h" 
 
@@ -12,12 +11,67 @@ int main()
 Bitmap image;
 vector <vector <Pixel> > bmp;
 Pixel rgb;
+string filename;
 
-image.open("green.bmp");
+ int avg;
+ int width;
+ int height;
+ bool cond = true;
+
+ while (cond)
+ {
+
+ cout<<"Enter the file name of an image( put extension .bmp after filename) : ";
+cin >> filename;
+
+image.open(filename);
+
+bool validBmp = image.isImage();
+
+if( validBmp == true )
+{
+
 bmp = image.toPixelMatrix();
 
-cout<<"green.bmp was uploaded. it's "<<bmp[0].size()<<" pixels wide and "<<bmp.size()<<" pixels high "<<endl;
+height = bmp.size();
 
+width = bmp[0].size();
+
+cout << filename <<" has been loaded. It is "<< width <<" pixels wide and "<<height<<" pixels high."<<endl;
+
+for(int i=0;i<bmp.size();i++)
+{
+
+   for(int j=0;j<bmp[i].size();j++)
+   {
+
+          rgb = bmp[i][j];
+
+   avg = (rgb.red +rgb.green+rgb.blue) / 3;
+
+       rgb.red = avg;
+
+       rgb.green = avg;
+
+       rgb.blue = avg;
+
+       bmp[i][j] = rgb;
+
+       }
+       }
+
+       image.fromPixelMatrix(bmp);
+
+       image.save("oldtimey.bmp");
+
+       cond = false;
+
+       }
+       else
+       {
+       cond = true;
+       }
+       }
 
 return 0;
 }
